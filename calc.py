fca729e615e57cfab2057a48138b678db91e9bdd
Nishidanit <nishidani-toshiki375@g.ecc.u-tokyo.ac.jp>
2020-06-15 15:24:23 +0900

    add multiple

--- calc.py
+++ calc.py
@@ -4,11 +4,15 @@ def sum(a, b):
 def subtract(a, b):
     return a - b
 
+def multiple(a, b):
+    return a * b
+
 val_1 = input('input first number: ')
 val_2 = input('input second number: ')
 
 value_sum = sum(int(val_1), int(val_2))
 value_subtract = subtract(int(val_1), int(val_2))
+value_multiple = multiple(int(val_1), int(val_2))
 print("result of sum = ") #改行
 
 
@@ -18,4 +22,9 @@ print("result of subtract = ") #改行
 
 
 print(value_subtract)
+
+print("result of multiple = ") #改行
+
+
+print(value_multiple)
 
