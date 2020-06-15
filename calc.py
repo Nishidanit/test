cd406511c5ff903efd073760e9d0c61044201f34
Nishidanit <nishidani-toshiki375@g.ecc.u-tokyo.ac.jp>
2020-06-15 14:41:21 +0900

    subtract

--- calc.py
+++ calc.py
@@ -1,10 +1,14 @@
 def sum(a, b):
     return a + b
 
+def subtract(a, b):
+    return a - b
+
 val_1 = input('input first number: ')
 val_2 = input('input second number: ')
 
 value_sum = sum(int(val_1), int(val_2))
+value_subtract = subtract(int(val_1), int(val_2))
 print("result of sum = ") #改行
 
 
